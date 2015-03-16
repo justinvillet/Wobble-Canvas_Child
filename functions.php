8b3c19d6d36c0b0873b22a071f4605d11ebb5ee5
richlloydmiles <richlloydmiles@gmail.com>
2015-03-16 08:39:38 +0200

    Added Font Awesome

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@
 function wobble_frontend_scripts() {
 	wp_enqueue_style( 'skeleton', get_stylesheet_directory_uri() . '/css/skeleton.css' );
 	wp_enqueue_style( 'normalize', get_stylesheet_directory_uri() . '/css/normalize.css' );
+	wp_enqueue_style( 'normalize', get_stylesheet_directory_uri() . '/css/font-awesome.min.css' );
 	// wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
 }
 
